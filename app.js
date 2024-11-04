e06f19e9def6e7f2a11dfe157f104247409bbeea
Kurt1-cyber <delutakurtlouie@gmail.com>
2024-11-04 19:07:40 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let fullName = 'Kurt Louie Deluta';
+let age = '19';
+let favNumber = 14;
+let favColor = 'red';
+
+console.log(fullName);
+console.log(age);
+console.log(favNumber);
+console.log(favColor);
